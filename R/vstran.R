fbe2f3f2c826a5c9bb721f96a752b9f250b78134
Tao Yang <xadmyangt@gmail.com>
2017-01-31 15:08:45 -0500

    adjust space

--- R/vstran.R
+++ R/vstran.R
@@ -17,12 +17,12 @@
 
 vstran  <- function(d){
 
-  x1r = rank(d[,1], ties.method = "random")
-  x2r = rank(d[,2], ties.method = "random")
-  x1.cdf.func = ecdf(x1r); x2.cdf.func = ecdf(x2r)
-  x1.cdf <- x1.cdf.func(x1r)
-  x2.cdf <- x2.cdf.func(x2r)
-  new_d=cbind(x1.cdf, x2.cdf)
+    x1r = rank(d[,1], ties.method = "random")
+    x2r = rank(d[,2], ties.method = "random")
+    x1.cdf.func = ecdf(x1r); x2.cdf.func = ecdf(x2r)
+    x1.cdf = x1.cdf.func(x1r)
+    x2.cdf = x2.cdf.func(x2r)
+    new_d = cbind(x1.cdf, x2.cdf)
 
-  return(new_d)
+    return(new_d)
 }

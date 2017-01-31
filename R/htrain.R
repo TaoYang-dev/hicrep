bde21d81a9fbe6acb2831eabcb122209ea905c52
Tao Yang <xadmyangt@gmail.com>
2017-01-31 14:07:05 -0500

    Update htrain.R

--- R/htrain.R
+++ R/htrain.R
@@ -12,6 +12,7 @@
 #' @referencesHiCRep: HiCRep: assessing the reproducibility of Hi-C data using a stratum-adjusted correlation coefficient. 
 #' Tao Yang, Feipeng Zhang, Galip Gurkan Yardimci, Ross C Hardison, William Stafford Noble, Feng Yue, Qunhua Li. 
 #' bioRxiv 101386; doi: https://doi.org/10.1101/101386.
+#' @importFrom stats cov cor
 #' @export
 #' @examples
 #' data(HiCR1)

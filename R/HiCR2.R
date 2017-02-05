6f6d62ee6191adcffbd999c992126ad1829641d1
Tao Yang <xadmyangt@gmail.com>
2017-02-05 16:31:35 -0500

    line length

--- R/HiCR2.R
+++ R/HiCR2.R
@@ -1,9 +1,13 @@
 #' @title HiCR2
-#' @description A Hi-C matrix in the format of \eqn{N*(3+N)}, the additional first three columns contains the chromosome
-#' information and coordinates of interaction bins. First is chromosome name, the second and third are the mid-points of
+#' @description A Hi-C matrix in the format of \eqn{N*(3+N)}, 
+#' the additional first three columns contains the chromosome
+#' information and coordinates of interaction bins. First is 
+#' chromosome name, the second and third are the mid-points of
 #' the contacting bins.
-#' @references  HiCRep: assessing the reproducibility of Hi-C data using a stratum-adjusted correlation coefficient. 
-#' Tao Yang, Feipeng Zhang, Galip Gurkan Yardimci, Ross C Hardison, William Stafford Noble, Feng Yue, Qunhua Li. 
+#' @references  HiCRep: assessing the reproducibility of Hi-C 
+#' data using a stratum-adjusted correlation coefficient. 
+#' Tao Yang, Feipeng Zhang, Galip Gurkan Yardimci, Ross C Hardison,
+#' William Stafford Noble, Feng Yue, Qunhua Li. 
 #' bioRxiv 101386; doi: https://doi.org/10.1101/101386.
 #'
 "HiCR2"

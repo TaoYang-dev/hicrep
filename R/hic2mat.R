5f50c08fb5afab97f136d8d6430a53612f451a98
Tao Yang <tao.yang@regeneron.com>
2020-02-24 16:08:47 -0500

    Fix documentation problem.

--- R/hic2mat.R
+++ R/hic2mat.R
@@ -1,20 +1,20 @@
 #' extract HiC data from .hic file and convert it to squared HiC matrix
 #'
 #' @param file the path to the .hic file.
-#' @param chromsome1 character number which specify chromsome to extract.
-#' @param chromsome2 for intra-chromsome data, set it same as chromosome 1; 
+#' @param chromosome1 character number which specify chromosome to extract.
+#' @param chromosome2 for intra-chromosome data, set it same as chromosome 1; 
 #' for inter-chromosome, set to another chromosome.
 #' @param resol resolution, i.e., bin size.
-#' @method specifies what data to extract, raw or normalized. Must be one of 
-#' NONE/VC/VC_SQRT/KR. NONE will gives raw counts. VC is vanilla coverage, 
-#' VC_SQRT is square root of vanilla coverage, and KR is Knight-Ruiz or 
-#' Balanced normalization.
+#' @param method specifies what data to extract, raw or normalized. 
+#' Must be one of "NONE", "VC", "VC_SQRT", "KR". "NONE" will gives raw counts. 
+#' VC is vanilla coverage, VC_SQRT is square root of vanilla coverage, and KR 
+#' is Knight-Ruiz or Balanced normalization.
 #' @return a squared HiC matrix that can be recongnized by get.scc.
 #' @references HiCRep: assessing the reproducibility of Hi-C data using a 
 #' stratum-adjusted correlation coefficient. Tao Yang, Feipeng Zhang, Galip
 #' Gurkan Yardimci, Fan Song, Ross C Hardison, William Stafford Noble, 
 #' Feng Yue, Qunhua Li. Genome Research 2017. doi: 10.1101/gr.220640.117
-#' @importFrom stawr staw
+#' @importFrom strawr straw
 #' @export
 
 
